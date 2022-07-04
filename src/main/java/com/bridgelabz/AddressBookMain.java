e17b23956b8fdddc6ece8f7da6fcc8801d56994e
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-07-05 00:25:46 +0530

    Added: Displaying welcome to Address Book Program

---
+++ src/main/java/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+/*
+ * @author: Nikhil Deshmukh
+ * Displaying Welcome message on the master branch
+ */
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
